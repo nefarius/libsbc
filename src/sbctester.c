e099c6cd43f717d22f4bda56dd2865304fac0480
Marcel Holtmann <marcel@holtmann.org>
2023-09-07 19:13:38 +0200

    src: Use SPDX-License-Identifier instead of license boilerplate

--- src/sbctester.c
+++ src/sbctester.c
@@ -1,26 +1,10 @@
 /*
+ * Bluetooth low-complexity, subband codec (SBC) library
+ * Copyright (C) 2007-2010  Marcel Holtmann <marcel@holtmann.org>
+ * Copyright (C) 2007-2008  Frederic Dalleau <fdalleau@free.fr>
+ * Copyright (C) 2008-2010  Nokia Corporation
  *
- *  Bluetooth low-complexity, subband codec (SBC) library
- *
- *  Copyright (C) 2008-2010  Nokia Corporation
- *  Copyright (C) 2007-2010  Marcel Holtmann <marcel@holtmann.org>
- *  Copyright (C) 2007-2008  Frederic Dalleau <fdalleau@free.fr>
- *
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 #ifdef HAVE_CONFIG_H
